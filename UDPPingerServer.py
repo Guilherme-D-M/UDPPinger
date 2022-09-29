12ee4b07217d233832f76062174242ccf817ff01
Guilherme Dal Más <77122294+Guilherme-D-M@users.noreply.github.com>
2022-09-29 10:07:37 -0300

    Update UDPPingerServer.py

--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -1,24 +1,25 @@
 # UDPPingerServer.py
 import random
 from socket import *
-# Create a UDP socket
+
+# Cria um soquete UDP
 # Observe o uso de SOCK_DGRAM para pacotes UDP
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-# Atribuir endereço IP e número de porta ao soquete
+# Atribui endereço IP e número de porta ao soquete
 serverSocket.bind(('', 1212))
 
 while True:
-    # Gerar número aleatório no intervalo de 0 a 10
+    # Gera um número aleatório no intervalo de 0 a 10
     rand = random.randint(0, 10)
 
-    # Receba o pacote do cliente junto com o endereço de onde ele está vindo
+    # Recebe o pacote do cliente junto com o endereço de onde ele está vindo
     message, address = serverSocket.recvfrom(1024)
     
-    # Capitalize a mensagem do cliente
+    # Deixa a mensagem em letras maiusculas
     message = message.upper()
 
     # Se rand for menor que 4, consideramos o pacote perdido e não respondemos
     if rand < 4:
         continue
-#Caso contrário, o servidor responde
+    #Caso contrário, o servidor responde
     serverSocket.sendto(message, address)
